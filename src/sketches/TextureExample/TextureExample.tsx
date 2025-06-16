3b4237ba6bc0706fc98eca548b8678947e8d7e15
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-06-16 15:05:38 -0700

    :books: comments

--- src/sketches/TextureExample/TextureExample.tsx
+++ src/sketches/TextureExample/TextureExample.tsx
@@ -11,14 +11,19 @@ const HelloWorld = (props: Props) => {
   const colorMap = useLoader(TextureLoader, "../images/avatar.jpg");
   return (
     <Canvas>
+      {/* FPS, OrbitControls */}
       <CanvasDebug />
 
       <ambientLight intensity={0.1} />
       <directionalLight position={[0, 0, 5]} />
+
+      {/* Textured plane */}
       <mesh rotation={[0, Math.PI / 4, Math.PI / 4]}>
         <planeGeometry />
         <meshStandardMaterial map={colorMap} />
       </mesh>
+
+      {/* Wireframe plane */}
       <mesh position={[0, 0, 0.1]} rotation={[0, Math.PI / 4, Math.PI / 4]}>
         <planeGeometry />
         <meshStandardMaterial wireframe color={COLOR_PRIMARY} />
